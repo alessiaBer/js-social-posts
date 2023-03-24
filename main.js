ea057fc36a42684287f4ebb35fffcd8264ab91a1
alessiaBer <alexdeyn@gmail.com>
2023-03-24 16:12:39 +0100

    [add] milestone 1 -> function to create posts

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
 Milestone 1
- Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Creiamo il nostro array di oggetti che rappresentano ciascun post.
 Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 id del post, numero progressivo da 1 a n
 nome autore,
@@ -10,15 +11,17 @@ testo del post,
 immagine (non tutti i post devono avere una immagine),
 numero di likes.
 Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
-Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+Milestone 2 
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
 BONUS
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 Formattare le date in formato italiano (gg/mm/aaaa)
 Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
 Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone */
 
-
-
+//Creiamo il nostro array di oggetti che rappresentano ciascun post
 const posts = [
     {
         "id": 1,
@@ -76,3 +79,57 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+/*** MILESTONE 1 ***/
+//seleziono l'elemento della DOM a cui aggiungere i posts
+const posts_container = document.querySelector('.posts-list');
+
+//invoco la funzione passando come parametri l'array posts e il posts_container
+createPost(posts, posts_container)
+
+/**
+ * funzione per creare posts e stamparli nella DOM
+ * @param {Array} array array di oggetti che contiene i posts da creare
+ * @param {Element} DOMel elemento della DOM a cui aggiungere il markup
+ */
+function createPost(array,DOMel) {
+    //itero nell'array di oggetti 
+    array.forEach((element, index) => {
+        //creo il markup che verrà assegnato ad ogni elemento dell'array e lo assegno ad una variabile
+        const postMarkup = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div> 
+        `;
+
+        //stampo il markup di ogni element dentro l'elemento della DOM
+        DOMel.innerHTML += postMarkup;
+    })
+}
